9890926b863d3adc8d0308f306577280ad96d512
ansrlgur12 <ansrlgur12@naver.com>
2023-02-11 17:10:16 +0900

    02.11 코테

---
+++ src/_02_11/Run.java
@@ -0,0 +1,10 @@
+package _02_11;
+
+public class Run {
+    public static void main(String[] args) {
+       Solution sol = new Solution();
+       System.out.println(sol.test1(626331));
+
+    }
+}
+
---
+++ src/_02_11/Solution.java
@@ -0,0 +1,18 @@
+package _02_11;
+
+public class Solution {
+        public int test1(long num) {
+            int answer = 0;
+            int cnt = 0;
+            while(num != 1) {
+                num = (num % 2 == 0) ? num / 2 : (num * 3) + 1;
+                cnt++;
+                System.out.print(cnt + " ");
+                System.out.println(num);
+                if(cnt > 500) {
+                    return answer = -1;
+                }
+            }
+            return answer = cnt;
+        }
+}
