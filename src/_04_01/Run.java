481a67c6370fe6af5fe110e66a1712700ff7212c
ansrlgur12 <ansrlgur12@naver.com>
2023-04-01 23:26:43 +0900

    0401

---
+++ src/_04_01/Run.java
@@ -0,0 +1,8 @@
+package _04_01;
+
+public class Run {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.test1(2, 3));
+    }
+}
---
+++ src/_04_01/Solution.java
@@ -0,0 +1,9 @@
+package _04_01;
+
+public class Solution {
+        public int test1(int num1, int num2) {
+            int answer = 0;
+            return answer = (num1 == num2) ? 1 : -1;
+        }
+
+}
