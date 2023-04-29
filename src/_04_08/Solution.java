325df778f04ac1e90f0af436825f8f9efc5c6a3c
ansrlgur12 <ansrlgur12@naver.com>
2023-04-29 18:37:08 +0900

    0409

--- src/_04_08/Solution.java
+++ src/_04_08/Solution.java
@@ -11,6 +11,7 @@ public class Solution {
         char[] a;
 
         for (int i = 0; i < str.length; i++){
+
             for (int j = 0; j < str[i].length(); j++) {
                 if (j == 0 || j % 2 == 0) {
                     chr = str[i].charAt(j) - 32;
